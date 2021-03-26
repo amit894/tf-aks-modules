3cd1e75ed8a5a81184746c2ba527851e46cb3112
amit894 <amit.894@gmail.com>
2021-03-27 01:09:21 +0530

    Initial AKS cluster checking=

--- main.tf
+++ main.tf
@@ -12,6 +12,85 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.rg_name}"
+  name     = "${var.prefix}-aks-rg"
   location = "${var.region}"
+}
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = "${var.vnet_cidr}"
+}
+
+resource "azurerm_subnet" "internal_subnet" {
+  name                 = "${var.prefix}-internal-subnet"
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
+  address_prefixes     = "${var.internal_subnet_cidr}"
+  enforce_private_link_endpoint_network_policies = true
+  enforce_private_link_service_network_policies = true
+
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = 1
+    vm_size        = "${var.default_node_pool_vm_size}"
+    vnet_subnet_id = azurerm_subnet.internal_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  api_server_authorized_ip_ranges = ["201.17.87.61/32"]
+
+  network_profile {
+   load_balancer_sku = "Standard"
+   network_plugin = "kubenet"
+   service_cidr = "192.168.0.0/17"
+   docker_bridge_cidr = "192.168.128.1/17"
+   dns_service_ip = "192.168.0.10"
+   }
+
+   tags = {
+       Environment = "Development"
+   }
+
+  addon_profile {
+    aci_connector_linux {
+      enabled = false
+    }
+
+    azure_policy {
+      enabled = false
+    }
+
+    http_application_routing {
+      enabled = false
+    }
+
+    kube_dashboard {
+      enabled = true
+    }
+
+    oms_agent {
+      enabled = false
+    }
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "user" {
+  name                  = "user"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "${var.user_node_pool_vm_size}"
+  node_count            = 1
+  vnet_subnet_id        = azurerm_subnet.internal_subnet.id
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,35 @@
-variable "rg_name" {
-  description = "RG Name which will be created"
+variable "prefix" {
+  description = "Prefix to be used for resources to will be created"
+  default = "test-app"
 }
 
 variable "region" {
   description = "Region name where rg will be hosted"
+  default = "eastus"
+}
+
+variable "vnet_cidr" {
+  description = "CIDR of the VNET for hosting"
+  default = ["10.0.0.0/16"]
+
+}
+
+variable "internal_subnet_cidr" {
+  description = "CIDR of the internal subnet for hosting"
+  default = ["10.0.0.0/24"]
+}
+
+variable "api_server_authorized_ip_ranges" {
+  description = "CIDR block of the Corporate CIDR"
+  default = ["201.17.87.61/32"]
+}
+
+variable "default_node_pool_vm_size" {
+  description = "Default Node Pool VM Size"
+  default = "Standard_D2_v2"
+}
+
+variable "user_node_pool_vm_size" {
+  description = "User Node Pool VM Size"
+  default = "Standard_D2_v2"
 }
