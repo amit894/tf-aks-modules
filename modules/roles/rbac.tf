7d7a7b340b7cfcc6eabd10952321c1dbeae3ff02
amit894 <amit.894@gmail.com>
2021-04-06 08:38:35 +0530

    Adding Roles and Assignments

--- modules/roles/rbac.tf
+++ modules/roles/rbac.tf
@@ -18,8 +18,47 @@ resource "azurerm_role_assignment" "aks_subnet" {
   principal_id         = var.principal_id
 }
 
-resource "azurerm_role_assignment" "aks_acr" {
-  scope                = var.container_registry_id
-  role_definition_name = "AcrPull"
-  principal_id         = var.kubelet_object_id
+resource "azurerm_role_definition" "network_admin" {
+  role_definition_id = "00000000-0000-0000-0000-000000000001"
+  name               = "network-admin-role"
+  scope              = var.network_resource_group_id
+
+  permissions {
+    actions     = ["Microsoft.Network/applicationGateways/read", "Microsoft.Network/applicationGateways/write", "Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/publicIPAddresses/join/action", "Microsoft.Network/publicIPPrefixes/join/action"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    var.network_resource_group_id,
+  ]
+}
+
+resource "azurerm_role_assignment" "network_admin_role_assignment" {
+  name               = "00000000-0000-0000-0000-000000000001"
+  scope              = var.network_resource_group_id
+  role_definition_id = azurerm_role_definition.network_admin.role_definition_resource_id
+  principal_id       = var.network_admin.object_id
+}
+
+
+resource "azurerm_role_definition" "cluster_admin" {
+  role_definition_id = "00000000-0000-0000-0000-000000000002"
+  name               = "cluster-admin-role"
+  scope              = var.cluster_resource_group_id
+
+  permissions {
+    actions     = ["Microsoft.Compute/diskEncryptionSets/read", "Microsoft.Compute/proximityPlacementGroups/write", "Microsoft.OperationalInsights/workspaces/sharedkeys/read,Microsoft.OperationalInsights/workspaces/read", "Microsoft.OperationsManagement/solutions/write", "Microsoft.OperationsManagement/solutions/read,Microsoft.ManagedIdentity/userAssignedIdentities/assign/action"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    var.cluster_resource_group_id,
+  ]
+}
+
+resource "azurerm_role_assignment" "cluster_admin_role_assignment" {
+  name               = "00000000-0000-0000-0000-000000000002"
+  scope              = var.cluster_resource_group_id
+  role_definition_id = azurerm_role_definition.cluster_admin.role_definition_resource_id
+  principal_id       = var.cluster_admin.object_id
 }
--- modules/roles/variables.tf
+++ modules/roles/variables.tf
@@ -13,3 +13,19 @@ variable "container_registry_id" {
 variable "kubelet_object_id" {
   description = "Object Id for the Kubelet for the AKS Cluster"
 }
+
+variable "network_resource_group_id" {
+  description = "Scope for Network Resource Group"
+}
+
+variable "cluster_resource_group_id" {
+  description = "Scope for Cluster Resource Group"
+}
+
+variable "network_admin.object_id {
+  description = "Identities for Network Admin Role"
+}
+
+variable "cluster_admin.object_id" {
+  description = "Identities for Cluster Admin Role"
+}
